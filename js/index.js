0b927df58235545905184479cafd7784278e6268
Lorena Nascimento <lorenanascimento28@gmail.com>
2021-11-10 10:05:56 -0300

    primeira versao da pagina profissional, apenas html, css e js

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function resumo(){
+    let frame = document.getElementById("frame");
+    frame.src="resumo.html"
+}
+
+function experiencias(){
+    let frame = document.getElementById("frame");
+    frame.src="experiencias.html"
+}
+
+function habilidades(){
+    let frame = document.getElementById("frame");
+    frame.src="habilidades.html"
+}
+
+function contatos(){
+    let frame = document.getElementById("frame");
+    frame.src="contatos.html"
+}
+
+function hello(){
+    let frame = document.getElementById("frame");
+    frame.src="helloworld.html"
+}
